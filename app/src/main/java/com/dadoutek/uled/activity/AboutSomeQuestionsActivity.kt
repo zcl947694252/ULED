47ddce5dab11a30d39864c6b374bef3383e47679
SawXu <Saw1993@126.com>
2018-07-04 10:21:18 +0800

    修改： Navgation back图片的名字

--- app/src/main/java/com/dadoutek/uled/activity/AboutSomeQuestionsActivity.kt
+++ app/src/main/java/com/dadoutek/uled/activity/AboutSomeQuestionsActivity.kt
@@ -15,7 +15,7 @@ class AboutSomeQuestionsActivity : AppCompatActivity() {
 
     private fun initView() {
         toolbar.title = getString(R.string.common_question)
-        toolbar.setNavigationIcon(R.drawable.navigation_back)
+        toolbar.setNavigationIcon(R.drawable.navigation_back_white)
         toolbar.setNavigationOnClickListener {
             finish()
         }
