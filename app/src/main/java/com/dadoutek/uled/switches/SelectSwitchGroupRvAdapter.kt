c1e76526385dc8834008b1973bcffbc0121db5e7
SawXu <Saw1993@126.com>
2018-07-06 18:24:09 +0800

    修改
    普通开关选择分组错乱。

--- app/src/main/java/com/dadoutek/uled/switches/SelectSwitchGroupRvAdapter.kt
+++ app/src/main/java/com/dadoutek/uled/switches/SelectSwitchGroupRvAdapter.kt
@@ -5,15 +5,29 @@ import com.chad.library.adapter.base.BaseViewHolder
 import com.dadoutek.uled.R
 import com.dadoutek.uled.model.DbModel.DbGroup
 
-class SelectSwitchGroupRvAdapter(layoutResId: Int, data: MutableList<DbGroup>?) : BaseQuickAdapter<DbGroup, BaseViewHolder>(layoutResId, data) {
-    var selectedPos = 0
-    override fun convert(helper: BaseViewHolder?, item: DbGroup?) {
-        helper?.setText(R.id.checkBox, item?.name)
-                ?.addOnClickListener(R.id.btnOff)
-                ?.addOnClickListener(R.id.btnOn)
-                ?.addOnClickListener(R.id.checkBox)
-        if (helper?.layoutPosition == selectedPos) {
-            helper.setChecked(R.id.checkBox, true)
+class SelectSwitchGroupRvAdapter(layoutResId: Int, data: MutableList<DbGroup>) : BaseQuickAdapter<DbGroup, BaseViewHolder>(layoutResId, data) {
+    var selectedPos = -1
+
+    init {
+        if (data.size > 0) {
+            for (i in data.indices) {
+                if (data[i].checked) {
+                    selectedPos = i
+                }
+            }
         }
     }
+
+    override fun convert(helper: BaseViewHolder?, item: DbGroup) {
+        helper?.setText(R.id.checkBox, item.name)
+                ?.addOnClickListener(R.id.checkBox)
+
+        if (helper?.layoutPosition == selectedPos) {
+            helper.setChecked(R.id.checkBox, true)
+        } else {
+            helper?.setChecked(R.id.checkBox, false)
+        }
+    }
+
+
 }
