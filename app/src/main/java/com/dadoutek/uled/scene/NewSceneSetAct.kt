1d749af18559f91207b04d906c8f0fe4efdec667
DDXexes <xexes.he@dadoutek.com>
2018-12-03 12:32:00 +0800

    sss

---
+++ app/src/main/java/com/dadoutek/uled/scene/NewSceneSetAct.kt
@@ -0,0 +1,23 @@
+package com.dadoutek.uled.scene
+
+import android.os.Bundle
+import com.dadoutek.uled.R
+import com.dadoutek.uled.tellink.TelinkBaseActivity
+
+class NewSceneSetAct :TelinkBaseActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_scene_set)
+        initData()
+        initView()
+    }
+
+    private fun initView() {
+
+    }
+
+    private fun initData() {
+
+    }
+}
