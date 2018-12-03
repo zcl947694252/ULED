2417c421a83bc3803192ed7615082f87d94b7611
DDXexes <xexes.he@dadoutek.com>
2018-12-03 14:12:22 +0800

    修改 新场景页修改迭代2

--- app/src/main/java/com/dadoutek/uled/scene/NewSceneSetAct.kt
+++ app/src/main/java/com/dadoutek/uled/scene/NewSceneSetAct.kt
@@ -1,11 +1,14 @@
 package com.dadoutek.uled.scene
 
 import android.os.Bundle
+import android.view.View
 import com.dadoutek.uled.R
+import com.dadoutek.uled.model.Constant
 import com.dadoutek.uled.tellink.TelinkBaseActivity
+import kotlinx.android.synthetic.main.activity_new_scene_set.*
 
 class NewSceneSetAct :TelinkBaseActivity(){
-
+    private var isChangeScene = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_scene_set)
@@ -14,10 +17,25 @@ class NewSceneSetAct :TelinkBaseActivity(){
     }
 
     private fun initView() {
+        if(isChangeScene){
+            showDataListView()
+        }else{
+            showEditListVew()
+        }
+    }
 
+    private fun showDataListView() {
+        data_view_layout.visibility= View.VISIBLE
+        edit_data_view_layout.visibility=View.GONE
+    }
+
+    private fun showEditListVew() {
+        data_view_layout.visibility= View.GONE
+        edit_data_view_layout.visibility=View.VISIBLE
     }
 
     private fun initData() {
-
+        val intent = intent
+        isChangeScene = intent.extras!!.get(Constant.IS_CHANGE_SCENE) as Boolean
     }
 }
