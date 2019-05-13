b448379bdf8d4e139e673f6c7fcf0bf0dd528ac4
yufeishang <695593266@qq.com>
2019-05-13 10:29:45 +0800

    修改： 修改组界面的显示

--- app/src/main/java/com/dadoutek/uled/fragment/CWLightFragmentList.kt
+++ app/src/main/java/com/dadoutek/uled/fragment/CWLightFragmentList.kt
@@ -1,15 +1,31 @@
 package com.dadoutek.uled.fragment
 
 import android.os.Bundle
+import android.support.constraint.ConstraintLayout
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.dadoutek.uled.R
+import com.dadoutek.uled.group.GroupListRecycleViewChildAdapter
+import com.dadoutek.uled.model.Constant
+import com.dadoutek.uled.model.DbModel.DBUtils
+import com.dadoutek.uled.model.DbModel.DbGradientBody
+import com.dadoutek.uled.model.DbModel.DbGroup
 import com.dadoutek.uled.othersview.BaseFragment
 
-class CWLightFragmentList: BaseFragment() {
+class CWLightFragmentList : BaseFragment() {
 
     private var inflater: LayoutInflater? = null
+
+    private var recyclerView: RecyclerView? = null
+
+    private var add_group: ConstraintLayout? = null
+
+    private var groupAdapter: GroupListAdapter? = null
+
+    private lateinit var groupList: ArrayList<DbGroup>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,12 +43,32 @@ class CWLightFragmentList: BaseFragment() {
     private fun getView(inflater: LayoutInflater): View {
         this.inflater = inflater
         val view = inflater.inflate(R.layout.group_list_fragment, null)
-
+        add_group = view.findViewById(R.id.no_group)
+        recyclerView = view.findViewById(R.id.group_recyclerView)
 
         return view
     }
 
     private fun initData() {
+        groupList = ArrayList()
 
+        val listAll = DBUtils.getAllGroupsOrderByIndex()
+        for (group in listAll) {
+            when (group.deviceType) {
+                Constant.DEVICE_TYPE_LIGHT_NORMAL->{
+                    groupList.add(group)
+                }
+                Constant.DEVICE_TYPE_DEFAULT_ALL->{
+                    groupList.add(group)
+                }
+            }
+        }
+
+        val layoutmanager = LinearLayoutManager(activity)
+        layoutmanager.orientation = LinearLayoutManager.VERTICAL
+        recyclerView!!.layoutManager = layoutmanager
+
+        this.groupAdapter = GroupListAdapter(R.layout.group_item_child, groupList)
+        groupAdapter!!.bindToRecyclerView(recyclerView)
     }
 }
---
+++ app/src/main/java/com/dadoutek/uled/fragment/GroupListAdapter.kt
@@ -0,0 +1,52 @@
+package com.dadoutek.uled.fragment
+
+import android.graphics.drawable.GradientDrawable
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+import com.chad.library.adapter.base.BaseItemDraggableAdapter
+import com.chad.library.adapter.base.BaseQuickAdapter
+import com.chad.library.adapter.base.BaseViewHolder
+import com.dadoutek.uled.R
+import com.dadoutek.uled.model.DbModel.DbGroup
+import com.dadoutek.uled.model.DbModel.DbLight
+import com.dadoutek.uled.tellink.TelinkLightApplication
+import com.dadoutek.uled.util.OtherUtils
+import com.dadoutek.uled.util.StringUtils
+
+class GroupListAdapter (layoutResId: Int, data: List<DbGroup>?) : BaseItemDraggableAdapter<DbGroup, BaseViewHolder>(layoutResId, data) {
+
+    override fun convert(helper: BaseViewHolder, group: DbGroup?) {
+
+        if (group != null) {
+            if (group.textColor == 0)
+                group.textColor = mContext.resources
+                        .getColor(R.color.black)
+            Log.d("setAddress", group.meshAddr.toString())
+            if (group.meshAddr == 0xffff) {
+                helper.setText(R.id.txt_name, TelinkLightApplication.getInstance().getString(R.string.allLight))
+            } else {
+                helper.setText(R.id.txt_name, group.name)
+//                if(OtherUtils.isCurtain(group)){
+//                    helper.setText(R.id.btn_set)
+//                }
+            }
+            if (OtherUtils.isCurtain(group)) {
+//                    helper.setText(R.id.btn_set)
+                helper.setVisible(R.id.btn_off, false)
+                helper.setVisible(R.id.btn_on, false)
+            }
+//            if(group.name==TelinkLightApplication.getInstance().getString(R.string.curtain)){
+//                helper.setVisible(R.id.btn_off,false)
+//                helper.setVisible(R.id.btn_on,false)
+//            }
+            helper.setTextColor(R.id.txt_name, group.textColor)
+                    .addOnClickListener(R.id.txt_name)
+                    .addOnClickListener(R.id.btn_on)
+                    .addOnClickListener(R.id.btn_off)
+                    .addOnClickListener(R.id.btn_set)
+//                    .addOnClickListener(R.id.add_group)
+        }
+
+    }
+}
